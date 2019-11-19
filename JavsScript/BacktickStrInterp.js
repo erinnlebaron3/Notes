d809f642544b64c4cf6873c30cf4b86b5b49ba6a
erinnlebaron <erinnlebaron3@gmail.com>
2019-11-19 13:34:39 -0700

    javascript

---
+++ JavsScript/BacktickStrInterp.js
@@ -0,0 +1,19 @@
+//https://mx.devcamp.com/full-stack-development-javascript-python/guide/1244
+
+const lyrics = 'Never gonna give you up';
+
+console.log(`I'm ${lyrics}`);
+"I'm never gonna give you up"
+
+const lyrics = 'Never gonna give you up';
+console.log(`I'm ${2 + 2}`);
+"I'm 4"
+
+const lyrics = 'Never gonna give you up';
+console.log(`I'm ${lyrics + " " + lyrics}`);
+"I'm never gonna give you up Never gonna give you up"
+
+
+//more modern versions of javascript uses the backticks
+//it's a little button right above the tab button on your keyboard
+
