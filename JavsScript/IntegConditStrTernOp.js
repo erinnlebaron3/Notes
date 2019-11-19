d482caa266b98394c26875057d3cf3f81ecc773f
erinnlebaron <erinnlebaron3@gmail.com>
2019-11-19 14:15:42 -0700

    javascript

---
+++ JavsScript/IntegConditStrTernOp.js
@@ -0,0 +1,13 @@
+// https://mx.devcamp.com/pt-full-stack-development-javascript-python-react/guide/1245
+
+
+const page = 'Home';
+console.log(`class=${ page === 'Home' ? 'master-layout' : 'secondary-layout' }`);
+
+// `${}` syntax
+
+// how to make a turnary opperator
+// conditional page === to home
+// master-layout if true
+// colon seperates
+// secondayry-layout false
